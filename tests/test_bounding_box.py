25b5eb0e51da1d9aeb4cf1eeb618787528d612f1
RafaelPadilla <rafael.padilla@smt.ufrj.br>
2020-12-24 16:15:47 -0300

    Removing not used files

--- tests/test_bounding_box.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from src.bounding_box import BoundingBox
-from src.utils.enumerators import BBType
-
-
-def test_input():
-    with pytest.raises(IOError):
-        BoundingBox("image", bb_type=BBType.DETECTED, confidence=None)
