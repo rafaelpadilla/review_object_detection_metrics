e68481db59ba7486b286a7f12df337d02d0c493c
RafaelPadilla <rafael.padilla@smt.ufrj.br>
2021-02-14 20:38:32 -0300

    Renaming folders with relative coordinates of the toyexample

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -3,7 +3,7 @@ import os
 import src.utils.converter as converter
 import src.utils.general_utils as general_utils
 import src.utils.validations as validations
-from src.utils.enumerators import BBType
+from src.utils.enumerators import BBFormat, BBType, CoordinatesType
 
 
 def test_converters_gts():
@@ -113,12 +113,78 @@ def test_converters_dets():
 
 
 def test_toy_example_dets():
-    dir_annots_dets = 'toyexample/dets_classid_rel_xywh'
+    dir_annots_dets = 'toyexample/dets_classid_rel_xcycwh'
 
     yolo_files = general_utils.get_files_recursively(dir_annots_dets)
     assert len(yolo_files) > 0
     for pascal_file in yolo_files:
         assert validations.is_yolo_format(pascal_file, bb_types=[BBType.DETECTED])
+
+
+def test_toy_example_dets_compatibility():
+    # Checks if all different formats in the toyexample represent the same coordinates
+    dir_img_dir = 'toyexample/images'
+    filepath_classes_det = 'toyexample/voc.names'
+
+    dir_dets_classid_abs_xywh = 'toyexample/dets_classid_abs_xywh'
+    dets_classid_abs_xywh = converter.text2bb(dir_dets_classid_abs_xywh,
+                                              bb_type=BBType.DETECTED,
+                                              bb_format=BBFormat.XYWH,
+                                              type_coordinates=CoordinatesType.ABSOLUTE)
+    dets_classid_abs_xywh = general_utils.replace_id_with_classes(dets_classid_abs_xywh,
+                                                                  filepath_classes_det)
+    dets_classid_abs_xywh.sort(key=lambda x: str(x), reverse=True)
+
+    dir_dets_classid_abs_xyx2y2 = 'toyexample/dets_classid_abs_xyx2y2'
+    dets_classid_abs_xyx2y2 = converter.text2bb(dir_dets_classid_abs_xyx2y2,
+                                                bb_type=BBType.DETECTED,
+                                                bb_format=BBFormat.XYX2Y2,
+                                                type_coordinates=CoordinatesType.ABSOLUTE)
+    dets_classid_abs_xyx2y2 = general_utils.replace_id_with_classes(dets_classid_abs_xyx2y2,
+                                                                    filepath_classes_det)
+    dets_classid_abs_xyx2y2.sort(key=lambda x: str(x), reverse=True)
+
+    dir_dets_classid_rel_xcycwh = 'toyexample/dets_classid_rel_xcycwh'
+    dets_classid_rel_xcycwh = converter.text2bb(dir_dets_classid_rel_xcycwh,
+                                                bb_type=BBType.DETECTED,
+                                                bb_format=BBFormat.YOLO,
+                                                type_coordinates=CoordinatesType.RELATIVE,
+                                                img_dir=dir_img_dir)
+    dets_classid_rel_xcycwh = general_utils.replace_id_with_classes(dets_classid_rel_xcycwh,
+                                                                    filepath_classes_det)
+    dets_classid_rel_xcycwh.sort(key=lambda x: str(x), reverse=True)
+
+    dir_dets_classname_abs_xywh = 'toyexample/dets_classname_abs_xywh'
+    dets_classname_abs_xywh = converter.text2bb(dir_dets_classname_abs_xywh,
+                                                bb_type=BBType.DETECTED,
+                                                bb_format=BBFormat.XYWH,
+                                                type_coordinates=CoordinatesType.ABSOLUTE)
+    dets_classname_abs_xywh.sort(key=lambda x: str(x), reverse=True)
+
+    dir_dets_classname_abs_xyx2y2 = 'toyexample/dets_classname_abs_xyx2y2'
+    dets_classname_abs_xyx2y2 = converter.text2bb(dir_dets_classname_abs_xyx2y2,
+                                                  bb_type=BBType.DETECTED,
+                                                  bb_format=BBFormat.XYX2Y2,
+                                                  type_coordinates=CoordinatesType.ABSOLUTE)
+    dets_classname_abs_xyx2y2.sort(key=lambda x: str(x), reverse=True)
+
+    dir_dets_classname_rel_xcycwh = 'toyexample/dets_classname_rel_xcycwh'
+    dets_classname_rel_xcycwh = converter.text2bb(dir_dets_classname_rel_xcycwh,
+                                                  bb_type=BBType.DETECTED,
+                                                  bb_format=BBFormat.YOLO,
+                                                  type_coordinates=CoordinatesType.RELATIVE,
+                                                  img_dir=dir_img_dir)
+    dets_classname_rel_xcycwh.sort(key=lambda x: str(x), reverse=True)
+
+    dir_dets_coco_format = 'toyexample/dets_coco_format'
+    dets_coco_format = converter.coco2bb(dir_dets_coco_format, bb_type=BBType.DETECTED)
+    dets_coco_format.sort(key=lambda x: str(x), reverse=True)
+
+    for a, b, c, d, e, f, g in zip(dets_classid_abs_xywh, dets_classid_abs_xyx2y2,
+                                   dets_classid_rel_xcycwh, dets_classname_abs_xywh,
+                                   dets_classname_abs_xyx2y2, dets_classname_rel_xcycwh,
+                                   dets_coco_format):
+        assert a == b == c == d == e == f == g
 
 
 def test_toy_example_gts():
