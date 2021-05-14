6dbd1f4e213d1f67d7929869275dca098f7bee77
DVMix <dvmixalkin@mail.ru>
2021-05-14 23:47:35 +0300

    .

--- supervisely/src/ui/ui.py
+++ supervisely/src/ui/ui.py
@@ -1,10 +1,13 @@
 import input
 import classes
 import settings
+import datasets
 
 
 def init(data, state):
     input.init(data, state)
     classes.init(data, state)
+    datasets.init(data, state)
     settings.init(data, state)
+
     pass
