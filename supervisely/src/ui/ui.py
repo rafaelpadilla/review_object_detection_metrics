e98504e4f115d90ac5196ac0aefa1a92953b7b18
DVMix <dvmixalkin@mail.ru>
2021-05-17 22:11:49 +0300

    .add conf_matrix tab

--- supervisely/src/ui/ui.py
+++ supervisely/src/ui/ui.py
@@ -2,6 +2,7 @@ import input
 import classes
 import settings
 import datasets
+import confusion_matrix
 
 
 def init(data, state):
@@ -9,5 +10,5 @@ def init(data, state):
     classes.init(data, state)
     datasets.init(data, state)
     settings.init(data, state)
-
+    confusion_matrix.init(data, state)
     pass
