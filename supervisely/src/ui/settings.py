e8555e7f192fd652988f5d5be7e1f2254e8468da
DVMix <dvmixalkin@mail.ru>
2021-05-19 09:05:09 +0300

    .connect sliders to calc

--- supervisely/src/ui/settings.py
+++ supervisely/src/ui/settings.py
@@ -1,7 +1,13 @@
 import supervisely_lib as sly
+import datasets as ds
 
 
 def init(data, state):
     state["samplePercent"] = 5
     state['IoUThreshold'] = 5
     state['ScoreThreshold'] = 45
+    total_img_num = 0
+    for k, v in ds.image_dict['gt_images'].items():
+        total_img_num += len(v)
+
+    data['totalImagesCount'] = total_img_num
