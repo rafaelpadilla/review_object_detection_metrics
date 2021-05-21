9607ccd4038cce4b3d0d99f86521377b61e26f78
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-05-21 13:37:14 +0300

    todo

--- supervisely/src/main.py
+++ supervisely/src/main.py
@@ -23,6 +23,25 @@ def main():
     ui.init(data, state)
     g.my_app.run(data=data, state=state)
 
+# @TODO: show-input в слайдеры и сделать их по размеру 450px
+# @TODO: Compare Datasets descr: += Only matched images are used in ....
+# @TODO: BUTTON_NAAME -> Calculate metrics
+# @TODO: обираем список selected classes
+# @TODO: подключить виджет галереи из SDK sly.app.widgets.CompareGallery
+
+# @TODO: Images for the selected cel in confusion matrix: bird (actual) <-> "cat" (predicted)
+# @TODO: DESCRIPTION 2 "bird" objects are detected as "dogs"
+# @TODO: DESCRIPTION 2 "bird" objects are not detected (bird <-> None)
+# @TODO: DESCRIPTION Model predicted 77 "cat" objects that are not in GT (None <-> Bird)
+# @TODO: Umar - скрол горизонтальный для confusion matrix - на стороне виджета?
+# @TODO: Umar - настройка толщины прямоугольников и полигонов в grid gallery
+# delete Image Statistic Table.
+# Image_mAP -> Metrics pre images 
+# Evaluate -> Preview
+# Extra Class Info -> убрать карточку
+# dog FP 10 vs 7 BUG
+# Extra Class Info -> cart to field, remove table
+# Sly Field - добавить перед таблицей для выбранного класса
 
 # @TODO: check requirements - two files instead of one
 # @TODO: disable class selection for conflicts
