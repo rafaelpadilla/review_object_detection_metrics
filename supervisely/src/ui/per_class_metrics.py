a1a9c48447c7c250e19511cff8ac6c1f4e9b4162
DVMix <dvmixalkin@mail.ru>
2021-05-25 18:58:13 +0300

    .add notification and gallery title

--- supervisely/src/ui/per_class_metrics.py
+++ supervisely/src/ui/per_class_metrics.py
@@ -6,11 +6,24 @@ import metrics
 
 def init(data, state):
     data['perClassExtendedTable'] = []
-    # data['perClassTable'] = {"columns": metrics.table_classes_columns, "data": []}
-    data['perClassLineChartOptions'] = {"title": "Line chart", "showLegend": True}
+    data['perClassTable'] = {"columns": metrics.table_classes_columns, "data": []}
+    data['perClassLineChartOptions'] = {"title": "Precision/Recall curve", "showLegend": True}
     data['perClassLineChartSeries'] = []
     data['perClassSingleImagesTable'] = {"columns": metrics.image_columns, "data": []}
     data['perClass'] = {}
+    data['perClassGalleryTitle'] = 'Please, select row from ImageTable.'
+    note_text = '''There may be differences between the Class table and the Image table. 
+    Such a situation is possible due to since when considering one single image, we do not pay 
+    attention to other class errors in other images from the set, but when evaluating the entire set, 
+    these errors are immediately accessible.'''
+    data['notification'] = {
+        "content": "##### {}".format(note_text),
+        "options": {
+            "name": "Important Note",
+            "type": "note"
+            }
+    }
+    data['perClassSelectedClsTitle'] = 'Class is not selected'
 
 
 def calculate_per_classes_metrics(api, task_id, src_list, dst_list, dst_project_name, method,
@@ -30,7 +43,7 @@ def calculate_per_classes_metrics(api, task_id, src_list, dst_list, dst_project_
     for tuple_ in table_classes:
         table_data.append(dict(className=tuple_[0], TP=tuple_[1], FP=tuple_[2], npos=tuple_[3],
                                Recall=tuple_[4], Precision=tuple_[5], AP=tuple_[6], tag='null'))
-    line_chart_options = {"title": "Line chart", "showLegend": True}
+    line_chart_options = {"title": "Precision/Recall curve", "showLegend": True}
     fields = [
         {"field": "data.perClassExtendedTable", "payload": table_data},
         # {"field": "data.perClassTable", "payload": {"columns": metrics.table_classes_columns,
@@ -53,14 +66,15 @@ def expand_line(data, class_name):
 
 def selected_class_metrics(api, task_id, src_list, dst_list, class_name, dst_project, iou_threshold, score_threshold):
     if class_name != 'ALL':
-        images_names_with_target_class = expand_line(src_list, class_name)
-
-        row_indexes = [id_ for id_, line in enumerate(src_list) if line[1] in images_names_with_target_class]
+        images_names_with_target_class = list(set(expand_line(src_list, class_name) + expand_line(dst_list, class_name)))
+        print('images_names_with_target_class =', images_names_with_target_class)
+        row_indexes1 = [id_ for id_, line in enumerate(src_list) if line[1] in images_names_with_target_class]
+        row_indexes2 = [id_ for id_, line in enumerate(dst_list) if line[1] in images_names_with_target_class]
+        row_indexes = list(set(row_indexes1 + row_indexes2))
+        print('row_indexes =', row_indexes)
         single_class_src_list_np = [src_list[id_] for id_ in row_indexes]
         single_class_dst_list_np = [dst_list[id_] for id_ in row_indexes]
 
-        single_class_src_list_np = single_class_src_list_np
-        single_class_dst_list_np = single_class_dst_list_np
     else:
         single_class_src_list_np = src_list
         single_class_dst_list_np = dst_list
@@ -90,11 +104,14 @@ def selected_class_metrics(api, task_id, src_list, dst_list, class_name, dst_pro
         target_chart = line_chart_series
         target_table = table_classes
 
+
+    perClassSelectedClsTitle = 'Selected class is "{}"'.format(class_name)
     fields = [
         {"field": "data.perClassSingleImagesTable", "payload": {"columns": metrics.image_columns,
                                                                 "data": class_images_pd_data}},
         # {"field": "data.perClassTable", "payload": {"columns": metrics.table_classes_columns,
-        #                                             "data": target_table}},
-        {"field": "data.perClassLineChartSeries", "payload": target_chart}
+        #                                             "data": target_table}},  # projects_pd_data
+        {"field": "data.perClassLineChartSeries", "payload": target_chart},
+        {"field": "data.perClassSelectedClsTitle", "payload": perClassSelectedClsTitle}
     ]
     api.app.set_fields(task_id, fields)
