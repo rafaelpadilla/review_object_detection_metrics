a1185ac16fd12c6fd2da026705a0ab4b380ef377
DVMix <dvmixalkin@mail.ru>
2021-05-25 18:58:50 +0300

    .add change threshold values

--- supervisely/src/ui/settings.py
+++ supervisely/src/ui/settings.py
@@ -4,8 +4,8 @@ import datasets as ds
 
 def init(data, state):
     state["samplePercent"] = 5
-    state['IoUThreshold'] = 5
-    state['ScoreThreshold'] = 45
+    state['IoUThreshold'] = 45
+    state['ScoreThreshold'] = 25
     total_img_num = 0
     for k, v in ds.image_dict['gt_images'].items():
         total_img_num += len(v)
