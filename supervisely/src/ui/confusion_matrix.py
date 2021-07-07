1a7c40a9b095d70002aa6a2f20a21d5c27115878
DVMix <dvmixalkin@mail.ru>
2021-07-07 16:16:21 +0300

    add ui text fixes

--- supervisely/src/ui/confusion_matrix.py
+++ supervisely/src/ui/confusion_matrix.py
@@ -5,6 +5,7 @@ import metrics
 from widgets.confusion_matrix import ConfusionMatrix
 from widgets.compare_gallery import CompareGallery
 from widgets.sly_table import SlyTable
+
 # from supervisely_lib.app.widgets.confusion_matrix import ConfusionMatrix
 
 confusion_matrix = ConfusionMatrix(api=g.api, task_id=g.task_id, v_model='data.slyConfusionMatrix')
@@ -29,6 +30,8 @@ def init(data, state):
     state["CMDisabled3"] = True
     state["CMShow3"] = False
 
+    data['CMImageTableTitle'] = "Statistic Image Table"
+
     conf_matrx_columns_v2 = []
     diagonal_max = 0
     max_value = 0
@@ -43,8 +46,7 @@ def init(data, state):
     data['slyConfusionMatrix'] = slyConfusionMatrix
     data['CMTableImages'] = {}
     data['CMGallery'] = {}
-    data['CMImageTableTitle'] = "Statistic Image Table"
-    data['CMImageTableDescription'] = "Cell is not selected."
+    data['CMImageTableDescription'] = "Please, select Cell from Confusion Matrix."
     data['CMGalleryTitle'] = 'Please, select row from Image Statistic Table.'
 
 
@@ -62,43 +64,49 @@ def reset_cm_state_to_default(api, task_id):
         {"field": "state.CMDisabled3", "payload": True},
         {"field": "state.CMShow3", "payload": False},
 
-        {"field": "state.CMActiveStep", "payload": None},
+        {"field": "state.CMActiveStep", "payload": 0},
 
-        {"field": "data.CMImageTableTitle", "payload": 'Cell is not selected.'},
-        {"field": "data.CMImageTableDescription1", "payload": 'Cell is not selected.'},
-        {"field": "data.CMGalleryTitle", "payload": 'Please, select row from ImageTable.'},
+        # {"field": "data.CMImageTableTitle", "payload": 'Cell is not selected.'},
+        {"field": "data.CMImageTableDescription1", "payload": 'Please, select Cell from Confusion Matrix.'},
+        {"field": "data.CMGalleryTitle", "payload": 'Please, select row from Image Statistic Table.'},
     ]
     api.app.set_fields(task_id, fields)
 
 
 # callback for CM image table
-@g.my_app.callback("show_image_table_cm")
+@g.my_app.callback("show_image_table")
 @sly.timeit
-def show_image_table_cm(api: sly.Api, task_id, context, state, app_logger):
+def show_image_table(api: sly.Api, task_id, context, state, app_logger):
     v_model = "data.CMTableImages"
 
     fields = [
-        {"field": "state.CMActiveStep", "payload": 2},
-        {"field": "state.CMCollapsed2", "payload": False},
-        {"field": "state.CMDisabled2", "payload": False},
+        # {"field": "state.CMActiveStep", "payload": 2},
+        # {"field": "state.CMCollapsed2", "payload": False},
+        # {"field": "state.CMDisabled2", "payload": False},
         {"field": "state.CMShow2", "payload": True},
-        {"field": "state.CMCollapsed3", "payload": True},
-        {"field": "state.CMDisabled3", "payload": True},
+        # {"field": "state.CMCollapsed3", "payload": True},
+        # {"field": "state.CMDisabled3", "payload": True},
         {"field": "state.CMShow3", "payload": False},
-        {"field": "data.CMGalleryTitle", "payload": 'Please, select row from ImageTable.'}
+
+        {"field": "data.CMImageTableDescription1", "payload": 'Please, select cell from Confusion Matrix.'},
+        {"field": "data.CMGalleryTitle", "payload": 'Please, select row from Image Statistic Table.'},
+        {"field": "state.CMActiveNames", "payload": ['confusion_matrix', 'image_stat_table']},
     ]
     api.app.set_fields(task_id, fields)
+    # print('selected row =', state['selected']['rowClass'])
+    # print('selected col =', state['selected']['colClass'])
     ui_utils.show_image_table_body(api, task_id, state, v_model, cm_image_table)
 
 
-@g.my_app.callback("show_images_confusion_matrix")
+@g.my_app.callback("show_images_gallery")
 @sly.timeit
-def show_images_confusion_matrix(api: sly.Api, task_id, context, state, app_logger):
+def show_images_gallery(api: sly.Api, task_id, context, state, app_logger):
     fields = [
-        {"field": "state.CMActiveStep", "payload": 3},
-        {"field": "state.CMCollapsed3", "payload": False},
-        {"field": "state.CMDisabled3", "payload": False},
+        # {"field": "state.CMActiveStep", "payload": 3},
+        # {"field": "state.CMCollapsed3", "payload": False},
+        # {"field": "state.CMDisabled3", "payload": False},
         {"field": "state.CMShow3", "payload": True},
+        {"field": "state.CMActiveNames", "payload": ['confusion_matrix', 'image_stat_table', 'grid_gallery']},
     ]
     api.app.set_fields(task_id, fields)
     ui_utils.show_images_body(api, task_id, state, gallery_conf_matrix, "data.CMGalleryTitle")
