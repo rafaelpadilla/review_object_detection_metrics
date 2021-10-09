2f6e885f29de87944e4ea50d7f7d75f34f8515fb
work <dvmixalkin@mail.ru>
2021-10-09 22:53:29 +0300

    add button -wip

--- supervisely/src/ui/input.py
+++ supervisely/src/ui/input.py
@@ -10,3 +10,12 @@ def init(data, state):
     data["predProjectId"] = g.pred_project_info.id
     data["predProjectName"] = g.pred_project_info.name
     data["predProjectPreviewUrl"] = g.api.image.preview_url(g.pred_project_info.reference_image_url, 100, 100)
+
+
+@g.my_app.callback("set_projects")
+@sly.timeit
+def set_projects(api: sly.Api, task_id, context, state, app_logger):
+    fields = [
+        {"field": "state.GlobalActiveStep", "payload": 2}
+    ]
+    api.app.set_fields(task_id, fields)
