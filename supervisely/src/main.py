49519185bfa4cb969e0641ec70da06ce32f4f5bb
work <dvmixalkin@mail.ru>
2021-10-09 22:54:05 +0300

    replace test example to ui

--- supervisely/src/main.py
+++ supervisely/src/main.py
@@ -9,67 +9,16 @@ def main():
         "context.teamId": g.team_id,
         "context.workspaceId": g.workspace_id
     })
-    try:
-        os.remove('db')
-    except:
-        pass
+    # try:
+    #     os.remove('db')
+    # except:
+    #     pass
 
     data = {}
     state = {}
 
-    # demo data
-    # ??? sort with empty cells
-    data["demoData"] = [
-        {
-            "gt": {
-                "class": "car",
-                "color": "#3F00FF",
-                "id": 777
-            },
-            "iou": 0.7,
-            "conf": 0.9999,
-            "pred": {
-                "class": "person",
-                "color": "#FBAD00",
-                "id": 888
-            },
-        },
-        {
-            "gt": {
-                "class": "dog",
-                "color": "#3F00FF",
-                "id": 111
-            },
-            "iou": 0,
-            "conf": 0,
-        },
-        {
-            "iou": 0,
-            "conf": 0.1,
-            "pred": {
-                "class": "cat",
-                "color": "#FBAD00",
-                "id": 222
-            },
-        },
-
-        # },
-        # {
-        #     "date": '2016-05-02',
-        #     "name": 'Tom',
-        #     "address": 'No. 189, Grove St, Los Angeles'
-        # },
-        # {
-        #     "date": '2016-05-04',
-        #     "name": 'Tom',
-        #     "address": 'No. 189, Grove St, Los Angeles'
-        # },
-        # {
-        #     "date": '2016-05-01',
-        #     "name": 'Tom',
-        #     "address": 'No. 189, Grove St, Los Angeles'
-        # }
-    ]
+    from supervisely.src.ui.test import test
+    test.init_demo_sample(data, state)
 
     # init data for UI widgets
     ui.init(data, state)
@@ -84,6 +33,7 @@ def main():
     # @TODO: check plt2bb in utils.py for bugs
 
     # @TODO: Umar - настройка толщины прямоугольников и полигонов в grid gallery - связаться с Антоном
+
 
 if __name__ == "__main__":
     sly.main_wrapper("main", main)
