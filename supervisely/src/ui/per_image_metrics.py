015ab89e2dbeb0ce32fa00becb41476b6e93d737
work <dvmixalkin@mail.ru>
2021-10-10 20:11:12 +0300

    None init for widgets

--- supervisely/src/ui/per_image_metrics.py
+++ supervisely/src/ui/per_image_metrics.py
@@ -7,8 +7,8 @@ import metrics
 import ui_utils
 # from supervisely_lib.app.widgets.sly_table import SlyTable
 
-image_sly_table = SlyTable(g.api, g.task_id, "data.perImageTable", g.image_columns)
-gallery_per_image = CompareGallery(g.task_id, g.api, 'data.perImage', g.aggregated_meta)
+image_sly_table = None  # SlyTable(g.api, g.task_id, "data.perImageTable", g.image_columns)
+gallery_per_image = None  # CompareGallery(g.task_id, g.api, 'data.perImage', g.aggregated_meta)
 
 
 def init(data, state):
@@ -19,8 +19,12 @@ def init(data, state):
 
 
 def calculate_per_image_metrics(api, task_id, gts, pred, method, iou_threshold, score_threshold):
+    global image_sly_table
     images_pd_data = metrics.calculate_image_mAP(gts, pred, method, target_class=None, iou=iou_threshold,
                                                  score=score_threshold, need_rez=False)
+    if image_sly_table is None:
+        image_sly_table = SlyTable(g.api, g.task_id, "data.perImageTable", g.image_columns)
+
     image_sly_table.set_data(images_pd_data)
     image_sly_table.update()
 
@@ -28,5 +32,9 @@ def calculate_per_image_metrics(api, task_id, gts, pred, method, iou_threshold, 
 @g.my_app.callback("show_images_per_image")
 @sly.timeit
 def show_images_per_image(api: sly.Api, task_id, context, state, app_logger):
+    global gallery_per_image
+    if gallery_per_image is None:
+        gallery_per_image = CompareGallery(g.task_id, g.api, 'data.perImage', g.aggregated_meta)
+
     ui_utils.show_images_body(api, task_id, state, gallery_per_image, "data.perImageGalleryTitle",
                               gallery_table='data.GalleryTable3')
