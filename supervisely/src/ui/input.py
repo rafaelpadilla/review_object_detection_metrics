287206147754eeaf80720218a5910b6a7402c98a
work <dvmixalkin@mail.ru>
2021-10-10 14:07:47 +0300

    update states

--- supervisely/src/ui/input.py
+++ supervisely/src/ui/input.py
@@ -3,19 +3,54 @@ import globals as g
 
 
 def init(data, state):
-    data["gtProjectId"] = g.gt_project_info.id
-    data["gtProjectName"] = g.gt_project_info.name
-    data["gtProjectPreviewUrl"] = g.api.image.preview_url(g.gt_project_info.reference_image_url, 100, 100)
+    state['GlobalInputCollapsed'] = False
+    state['GlobalInputDisabled'] = False
+    state['doneInput'] = False
+    state['InputInProgress'] = True
 
-    data["predProjectId"] = g.pred_project_info.id
-    data["predProjectName"] = g.pred_project_info.name
-    data["predProjectPreviewUrl"] = g.api.image.preview_url(g.pred_project_info.reference_image_url, 100, 100)
+    # data["gtProjectId"] = g.gt_project_info.id
+    # data["gtProjectName"] = g.gt_project_info.name
+    # data["gtProjectPreviewUrl"] = g.api.image.preview_url(g.gt_project_info.reference_image_url, 100, 100)
+    #
+    # data["predProjectId"] = g.pred_project_info.id
+    # data["predProjectName"] = g.pred_project_info.name
+    # data["predProjectPreviewUrl"] = g.api.image.preview_url(g.pred_project_info.reference_image_url, 100, 100)
+
+
+def restart(data, state):
+    data = None
+    state = None
 
 
 @g.my_app.callback("set_projects")
 @sly.timeit
 def set_projects(api: sly.Api, task_id, context, state, app_logger):
+    state['GlobalInputCollapsed'] = True
+    state['GlobalInputDisabled'] = False
+    state['doneInput'] = True
+    state['InputInProgress'] = False
+
+    state['GlobalDatasetsCollapsed'] = True
+    state['GlobalDatasetsDisabled'] = True
+    state['doneDatasets'] = False
+    state['DatasetsInProgress'] = True
+
+    gt_project_info = api.project.get_info_by_id(state['gtProjectId'], raise_error=True)
+    pr_project_info = api.project.get_info_by_id(state['predProjectId'], raise_error=True)
+
     fields = [
-        {"field": "state.GlobalActiveStep", "payload": 2}
+        {"field": "state.GlobalActiveStep", "payload": 2},
+        {"field": "data.gtProjectId", "payload": gt_project_info.id},
+        {"field": "data.gtProjectName", "payload": gt_project_info.name},
+        {"field": "data.gtProjectPreviewUrl",
+         "payload": g.api.image.preview_url(gt_project_info.reference_image_url, 100, 100)},
+
+        {"field": "data.predProjectId", "payload": pr_project_info.id},
+        {"field": "data.predProjectName", "payload": pr_project_info.name},
+        {"field": "data.predProjectPreviewUrl",
+         "payload": g.api.image.preview_url(pr_project_info.reference_image_url, 100, 100)},
+
+        {"field": "state.doneInput", "payload": True},
+
     ]
     api.app.set_fields(task_id, fields)
