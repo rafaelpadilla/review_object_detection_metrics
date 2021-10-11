5375c2637982af4be003f37badfba321d98dd56b
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-10-11 07:35:26 +0300

    clean main

--- supervisely/src/main.py
+++ supervisely/src/main.py
@@ -1,7 +1,6 @@
 import supervisely_lib as sly
 import globals as g
 import ui
-import os
 
 
 def main():
@@ -9,10 +8,6 @@ def main():
         "context.teamId": g.team_id,
         "context.workspaceId": g.workspace_id
     })
-    # try:
-    #     os.remove('db')
-    # except:
-    #     pass
 
     data = {}
     state = {}
@@ -25,9 +20,7 @@ def main():
     g.my_app.compile_template(g.root_source_dir)
     g.my_app.run(data=data, state=state)
 
-    #@TODO: GTteamId - gtProjectId - нужно сделать именования везде однородные, начинаем с маленькой буквы
-    #@TODO:
 
-
+# @TODO: GTteamId - gtProjectId - camel case
 if __name__ == "__main__":
     sly.main_wrapper("main", main)
